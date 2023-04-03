fc11ca59e2dc9bc9deafa25da8813ccf871fbfaa
DiegoLG15 <a20200839@pucp.edu.pe>
2023-04-03 17:09:05 -0500

    First Commit

---
+++ PracticaLab2/src/main/java/com/example/practicalab2/PracticaLab2Application.java
@@ -0,0 +1,13 @@
+package com.example.practicalab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaLab2Application.class, args);
+    }
+
+}
